d251ab3dcbd3f05de457053c3525e2e60602522b
medavidrahmadana <me.davidrahmadana@gmail.com>
2023-03-18 10:06:03 +0700

    Menambahkan class DataGeneric

--- tpmodul5_1302213079/Program.cs
+++ tpmodul5_1302213079/Program.cs
@@ -6,11 +6,29 @@
     }
 }
 
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        HaloGeneric hg = new HaloGeneric();
-        hg.SapaUser("David");
+        HaloGeneric haloG = new HaloGeneric();
+        haloG.SapaUser("David");
+        DataGeneric<string> dataG = new DataGeneric<string>("1302213079");
+        dataG.PrintData();
     }
 }
